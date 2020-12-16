ef12d5de3239a023f684c633eeeebef736ddc669
lanycsq <shanqiong118@163.com>
2020-12-16 14:34:29 +0800

    init

---
+++ src/js/app2.js
@@ -0,0 +1,16 @@
+import '../css/app2.css'
+import $ from 'jquery'
+
+const $tabBar = $("#app2 .tab-bar")
+const $tabContent = $("#app2 .tab-content")
+
+$tabBar.on('click', "li", e => {
+    const $li = $(e.currentTarget);
+    const index = $li.index();
+    $li.addClass('selected')
+        .siblings().removeClass('selected')
+    $tabContent.children().eq(index).addClass('active')
+        .siblings().removeClass('active')
+});
+//设置一开始就点击一次
+$tabBar.children().eq(0).trigger('click')
---
+++ src/js/app3.js
@@ -0,0 +1,8 @@
+import $ from 'jquery'
+import '../css/app3.css'
+
+const $square = $("#app3 .square")
+
+$square.on("click", () => {
+    $square.toggleClass('active')
+})
